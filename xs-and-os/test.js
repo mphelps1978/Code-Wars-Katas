0ea961346b540883009773abe0195c55800ff2b7
mphelps1978 <56881040+mphelps1978@users.noreply.github.com>
2020-03-11 08:50:20 -0500

    XS and OS Solution

--- xs-and-os/test.js
+++ xs-and-os/test.js
@@ -1,5 +1,27 @@
 const XO = (str) => {
+  let splitString = str.split("")
+  let xs = []
+  let os = []
 
+  splitString.map(x => {
+    if(x === 'x' || x === 'X') {
+      xs.push(x)
+    }
+    if(x === 'o' || x === 'O') {
+      os.push(x)
+    }
+
+  })
+  console.log(xs.length)
+  console.log(os.length)
+
+  if (xs.length === os.length) {
+    return true
+  } else if (xs.length != os.length){
+      return false
+  } else {
+      return true
+  }
 }
 
 console.log(XO("xxOo")); //true
