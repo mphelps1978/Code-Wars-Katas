83eae379dff54a737b363f415b0ca705dd8ceb8a
Michael Phelps <56881040+mphelps1978@users.noreply.github.com>
2020-03-11 13:01:26 -0500

    Update test.js

--- xs-and-os/test.js
+++ xs-and-os/test.js
@@ -10,11 +10,7 @@ const XO = (str) => {
     if(x === 'o' || x === 'O') {
       os.push(x)
     }
-
   })
-  console.log(xs.length)
-  console.log(os.length)
-
   if (xs.length === os.length) {
     return true
   } else if (xs.length != os.length){
